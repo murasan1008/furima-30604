37497b67b40458ebc675a28947e361531214ae6b
murasan1008 <acm1899taka1008@icloud.com>
2020-10-20 19:30:03 +0900

    Rubocopを実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"taka1008"}
-    password_confirmation {password}
-    last_name             {"貴光"}
-    first_name            {"村田"}
-    last_name_kana        {"タカミツ"}
-    first_name_kana       {"ムラタ"}
-    birthday              {"1985-10-8"}
+    nickname              { 'abe' }
+    email                 { 'kkk@gmail.com' }
+    password              { 'taka1008' }
+    password_confirmation { password }
+    last_name             { '貴光' }
+    first_name            { '村田' }
+    last_name_kana        { 'タカミツ' }
+    first_name_kana       { 'ムラタ' }
+    birthday              { '1985-10-8' }
   end
 end
