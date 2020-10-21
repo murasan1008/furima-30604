37f264cd9b592754dc2f9cf64c532e68310d477a
murasan1008 <acm1899taka1008@icloud.com>
2020-10-21 16:09:27 +0900

    商品出品ページを編集

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,5 @@
 class Status < ActiveHash::Base
-  self.date = [
+  self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '新品、未使用' },
     { id: 3, name: '未使用に近い' },
@@ -8,5 +8,5 @@ class Status < ActiveHash::Base
     { id: 6, name: '傷や汚れあり' },
     { id: 7, name: '全体的に状態が悪い' }
   ]
- end
+end
  
