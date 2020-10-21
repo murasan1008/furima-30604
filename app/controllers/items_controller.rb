a36a80dfe8d7def0e275d770373aaa58e75b4f37
murasan1008 <acm1899taka1008@icloud.com>
2020-10-21 16:36:59 +0900

    ストロングパラメーターを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  
+
   def index
   end
 
@@ -9,5 +9,14 @@ class ItemsController < ApplicationController
   end
 
   def create
+    
+  end
+
+
+private
+  def item_params
+    params.require(:ieme).permit(:name, :description, :genre_id, :status_id,
+      :ship_burden_id, :shipment_source_id, :days_to_id, :price).
+        mage(user_id: current_user.id)
   end
 end
