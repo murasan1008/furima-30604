04ebcc126dd410210b26bf3334bca61f9f90d422
murasan1008 <acm1899taka1008@icloud.com>
2020-10-21 11:58:45 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipment_source
   belongs_to_active_hash :days_to
   has_one_attached :image
+
+  validates :image,            presence: { message: "入力された文字が空欄です" }
+  validates :name,             presence: { message: "入力された文字が空欄です" }
+  validates :description,      presence: { message: "入力された文字が空欄です" }
+  validates :price,            presence: { message: "入力された文字が空欄です" }
+  with_options presence: { message: "入力された文字が空欄です" }, numericality: { other_than: 1, message: '選択して下さい' } do
+    validates :genre
+    validates :status
+    validates :ship_burden
+    validates :shipment_source
+    validates :days_to
+  end
+  validates :price, inclusion: { in: 300..9999999, message: '規定価格の範囲内で入力して下さい' },
+    format: { with: /\A[0-9]+\z/, message: '規定価格の範囲内で入力して下さい'}
 end
