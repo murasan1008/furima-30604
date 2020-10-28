ca466a4a297518b8550fd6eaa5a4b0f56d323ab4
murasan1008 <acm1899taka1008@icloud.com>
2020-10-28 18:41:38 +0900

    ストロングパラメーターを設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,4 +9,10 @@ class OrdersController < ApplicationController
 
   def create
   end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :ship_region_id, :ship_city, :ship_block, :ship_building, :phone,).merge(
+      user_id: current_user.id, item_id: params[:item_id])
+  end
 end
