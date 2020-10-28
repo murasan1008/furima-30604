77c9f9da197d07a66798790f8547cf40278c2955
murasan1008 <acm1899taka1008@icloud.com>
2020-10-28 23:26:18 +0900

    createアクションを編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,18 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-  end
-
-  def new
     @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+     redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render action: :index
+    end
   end
 
   private
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,4 @@
-class OderAddress
+class OrderAddress
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :postal_code, :ship_region_id, :ship_city, :ship_block,
                 :ship_building, :phone, :order
@@ -7,12 +7,12 @@ class OderAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: '半角数字、又は-を付けて入力して下さい'}
     validates :ship_city
     validates :ship_block
-    validates :phone, format: {with: ^0\d{9,10}$, message: '半角数字、又は-を付けて入力して下さい'}
-    validates :ship_region_id, numericality: { other_than: 1 message: '選択して下さい'}
+    validates :phone, format: {with: /\A\d{11}\z/, message: '半角数字、又は-を付けないで入力して下さい'}
+    validates :ship_region_id, numericality: { other_than: 1, message: '選択して下さい'}
   end
 
   def save
-    Order.create(user_id: current_user.id, item_id: params[:item_id])
+    order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(order_id: order.id, postal_code: postal_code, ship_region_id: ship_region_id, ship_city: ship_city,
                    ship_block: ship_block, ship_building: ship_building, phone: phone)
   end
